cac04f8c225113cfd9c072a758f2324724dee49c
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-01-30 16:52:16 +0100

    Set up a SQLite3 database

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '',
 	'username' => '',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'database' => APPPATH . 'database/noel.db',
+	'dbdriver' => 'sqlite3',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
