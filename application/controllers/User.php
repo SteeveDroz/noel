e661fef8178ef4a094dc386d5ba2bd818ba1a66e
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-01-30 06:32:49 +0100

    Set up the base of the app

---
+++ application/controllers/User.php
@@ -0,0 +1,11 @@
+<?php
+
+class User extends CI_Controller {
+
+  public function index() {
+    $this->load->view('header');
+    $this->load->view('footer');
+  }
+}
+
+ ?>
---
+++ application/views/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo isset($title) ? $title . ' - Noel' : 'Noel' ?></title>
+  </head>
+  <body>
